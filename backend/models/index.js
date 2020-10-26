0a5d69f3b466d77466ef351c8b85e6b306d9aadb
Razvan Turturica <razvan.turtu@yahoo.com>
2020-10-26 02:04:30 +0200

    Debug backend CI

--- backend/models/index.js
+++ backend/models/index.js
@@ -10,6 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+    console.log(process.env);
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
     sequelize = new Sequelize(
